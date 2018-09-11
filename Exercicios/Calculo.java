6d9217908d3c72c37360552c22b730e66de142d9
AnaBertolucci <anabertolucci14@gmail.com>
2018-09-10 22:38:13 -0300

    java

---
+++ Exercicios/Calculo.java
@@ -0,0 +1,17 @@
+package aula;
+
+public class Calculo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int r = 12;
+		double pi = 3.1415;
+		double a = pi * (r * r);
+		double divi = (99 % 4);
+		int num  = 7;
+		int mul = num+num+num+num+num+num+num+num;
+		System.out.println(mul);
+		
+		}
+	}
+
